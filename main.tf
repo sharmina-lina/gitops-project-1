b9f418729e55d2e30b9aac89563c38a46f1869f7
sharmina-lina <lina_cse02@yahoo.com>
2024-11-19 12:45:09 +0100

    update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,13 @@ provider "aws" {
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "sharmina_bucket_S3_testing"
+}
+
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.bucket
   acl    = "private"
 }
+
 
 output "bucket_name" {
   value = aws_s3_bucket.my_bucket.bucket
